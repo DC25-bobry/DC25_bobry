65e2934e149ed49feebb03d8a1c92d5af8da81c8
hubertleszcz <hubciol1308@gmail.com>
2025-11-02 18:42:57 +0100

    Created logic for file validation and updated route

--- backend/src/api/routes/upload_route.py
+++ backend/src/api/routes/upload_route.py
@@ -1,10 +1,42 @@
-from fastapi import APIRouter, UploadFile, File
+import logging
+import threading
 
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import HTMLResponse
+from backend.src.api.utils.file_validation import validate_file
+from backend.src.api.services.document_parsing.document_parsing_service import DocumentParsingService
+
+def process_file(file_bytes: bytes, filename: str, content_type: str):
+    parsing_service = DocumentParsingService()
+    parsed_document = parsing_service.extract_text(content=file_bytes, content_type=content_type, filename=filename)
 
 router = APIRouter()
 
-@router.get("/upload")
-async def uploadFile(file: UploadFile = File(...)):
-    return await file.read()
+@router.get("/upload", response_class=HTMLResponse)
+async def upload_form():
+    return """
+    <!doctype html>
+    <title>Upload new Files</title>
+    <h1>Upload new Files</h1>
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="files" multiple required>
+      <input type="submit" value="Upload">
+    </form>
+    </html>
+    """
 
+@router.post("/upload", response_class=HTMLResponse)
+async def upload_files(files: list[UploadFile] = File(...)):
+    if not files:
+        raise HTTPException(status_code=400, detail="No files sent")
 
+    for file in files:
+        file_bytes = await file.read()
+
+        file_valid, info = validate_file(file_bytes, file.filename, file.content_type)
+        if not file_valid:
+            raise HTTPException(status_code=400, detail=info)
+
+        thread = threading.Thread(target=process_file, args=(file_bytes, file.filename, file.content_type,))
+        thread.start()
+    return HTMLResponse(content=f"Parsing {len(files)} files.")
---
+++ backend/src/api/utils/file_validation.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+allowed_MIME = {
+    "application/pdf": ".pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx"
+}
+
+max_size = 1024 * 1024 * 50 # 2 MB
+
+def validate_file(file_bytes: bytes, filename: str, content_type: str) -> Tuple[bool, str]:
+    message = f"{filename}: "
+    valid = True
+    if len(file_bytes) > max_size:
+        message += "Wrong file size. "
+        valid = False
+    if content_type not in allowed_MIME:
+        message += "Wrong content type."
+        valid = False
+
+    if valid:
+        message += "OK"
+
+    return valid, message
