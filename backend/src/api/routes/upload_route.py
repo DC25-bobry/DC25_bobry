ed8636356b7da48aed7629cdd8d039fe0a7f9565
hubertleszcz <hubciol1308@gmail.com>
2025-11-03 17:13:57 +0100

    deleted redundant HTTPException

--- backend/src/api/routes/upload_route.py
+++ backend/src/api/routes/upload_route.py
@@ -6,8 +6,7 @@ from fastapi.responses import HTMLResponse
 from backend.src.api.utils.file_validation import validate_file
 from backend.src.api.services.document_parsing.document_parsing_service import DocumentParsingService
 
-def process_file(file_bytes: bytes, filename: str, content_type: str):
-    parsing_service = DocumentParsingService()
+def process_file(file_bytes: bytes, filename: str, content_type: str, parsing_service: DocumentParsingService):
     parsed_document = parsing_service.extract_text(content=file_bytes, content_type=content_type, filename=filename)
 
 router = APIRouter()
@@ -30,13 +29,22 @@ async def upload_files(files: list[UploadFile] = File(...)):
     if not files:
         raise HTTPException(status_code=400, detail="No files sent")
 
+    parsing_service = DocumentParsingService()
+    error_count = 0
+    wrong_files = ""
+
     for file in files:
         file_bytes = await file.read()
 
         file_valid, info = validate_file(file_bytes, file.filename, file.content_type)
         if not file_valid:
-            raise HTTPException(status_code=400, detail=info)
+            error_count += 1
+            wrong_files += file.filename
+        else:
+            thread = threading.Thread(target=process_file, args=(file_bytes, file.filename, file.content_type, parsing_service))
+            thread.start()
 
-        thread = threading.Thread(target=process_file, args=(file_bytes, file.filename, file.content_type,))
-        thread.start()
-    return HTMLResponse(content=f"Parsing {len(files)} files.")
+    response_content = f"Parsing {len(files) - error_count} files."
+    if error_count > 0:
+        response_content += f"\n{wrong_files}"
+    return HTMLResponse(content=response_content)
