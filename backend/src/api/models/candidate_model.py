f4d8a24d48b9c5cb2d7e5bc6f277ff81c4e19813
hubertleszcz <hubciol1308@gmail.com>
2025-11-11 17:15:47 +0100

    Added pydantic models for candidate and job offer

---
+++ backend/src/api/models/candidate_model.py
@@ -0,0 +1,48 @@
+from typing import Optional, List
+from pydantic import BaseModel, ConfigDict, EmailStr, HttpUrl
+
+
+class EducationModel(BaseModel):
+    school_name: str
+    start_year: int
+    end_year: int
+    field_of_study: str
+
+    model_config = {"extra": "forbid"}
+
+class ExperienceModel(BaseModel):
+    company_name: str
+    start_year: int
+    end_year: int
+    position: str
+
+    model_config = {"extra": "forbid"}
+
+class DentalParametersModel(BaseModel):
+    valid_dental_checkup: bool
+    teeth_spacing: float
+
+    model_config = {"extra": "forbid"}
+
+class SkillModel(BaseModel):
+    name: str
+    category: str
+    proficiency_level: int
+
+    model_config = {"extra": "forbid"}
+
+class CandidateModel(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+    phone: str
+    linkedin: Optional[HttpUrl] = None
+
+    education: Optional[List[EducationModel]] = None
+    experience: Optional[List[ExperienceModel]] = None
+    dental_parameters: DentalParametersModel
+
+    health_certificate: bool
+    skills: Optional[List[SkillModel]] = None
+
+    model_config = {"extra": "forbid"}
